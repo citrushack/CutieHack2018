ec003e77785983012a39705f7cbd30ba1ac10bc8
Gabriel Gomez <gabrielgomez@Gabriels-MacBook-Pro.local>
2018-08-07 16:56:54 -0700

    added jquery file, started jquery for FAQ section

---
+++ cutie/landingapp/static/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $("#q1").click(function(){
+        $("#a1").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#q2").click(function(){
+        $("#a2").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#q3").click(function(){
+        $("#a3").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#q4").click(function(){
+        $("#a4").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#q5").click(function(){
+        $("#a5").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#q6").click(function(){
+        $("#a6").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#q7").click(function(){
+        $("#a7").slideToggle("slow");
+    });
+});
