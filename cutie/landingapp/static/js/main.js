50a16e11de0a5df135dd98eb80798780009884c8
Gabriel Gomez <gabrielgomez@Gabriels-MBP.socal.rr.com>
2018-08-21 01:13:35 -0700

    added comments to main.js file for ease of reading

--- cutie/landingapp/static/js/main.js
+++ cutie/landingapp/static/js/main.js
@@ -9,13 +9,21 @@ $(document).ready(function(){
 
 //jQuery navbar smooth scrolling
 $(document).ready(function(){
+    //when a tag with a valid anchortag is clicked
     $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+        //if the location is the same as the one that is clicked
         if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            //store the href as 'target'
             var target = $(this.hash);
+
+            //ion't really know what the slice does but this basically checks if the target isn't empty
             target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
             
             if (target.length) {
+                //prevent default jump scroll
                 event.preventDefault();
+
+                //jQuery smooth scroll magic 
                 $("html, body").animate(
                     {
                         scrollTop: target.offset().top
@@ -39,8 +47,11 @@ $(document).ready(function(){
 
 //changes active nav link on click
 $(document).ready(function() {
+    //once you click a nav link
     $('nav ul li a').click(function() {
+        //remove 'active' class from all nav links
         $('nav ul li a').removeClass();
+        //add 'active' class to the nav link that was clicked (this)
         $(this).addClass('active');
     });
 });
@@ -48,30 +59,30 @@ $(document).ready(function() {
 //changes active nav link on scroll
 $(document).ready(function() {
     $(window).scroll(function(){
-        var scrollTop       = $(window).scrollTop() + 10,
+        var scrollTop       = $(window).scrollTop() + 10, // top of browser + 10px
             whatisOffset    = $('#whatis').offset().top,
-            whatisDist      = (whatisOffset - scrollTop),
+            whatisDist      = (whatisOffset - scrollTop), // stores current distance between top of browser and "about" section
             faqOffset       = $('#faq').offset().top,
-            faqDist         = (faqOffset - scrollTop),
+            faqDist         = (faqOffset - scrollTop), // stores current distance between top of browser and "faq" section
             sponsorsOffset  = $('#sponsors').offset().top,
-            sponsorsDist    = (sponsorsOffset - scrollTop),
+            sponsorsDist    = (sponsorsOffset - scrollTop), // stores current distance between top of browser and "sponsors" section
             organizersOffset   = $('#organizers').offset().top;
-            organizersDist     = (organizersOffset - scrollTop);
+            organizersDist     = (organizersOffset - scrollTop); // stores current distance between top of browser and "contact" section
 
-        if (whatisDist > 0) {
+        if (whatisDist > 0) { //checks if you're in home section
             $('nav ul li a').removeClass();   
             $('#homelink').addClass('active');
-        } else if (whatisDist <= 0 && faqDist > 0) {
+        } else if (whatisDist <= 0 && faqDist > 0) { //checks if you're in about section
             $('nav ul li a').removeClass();
             $('#aboutlink').addClass('active');
-        } else if (faqDist <= 0 && sponsorsDist > 0) {
+        } else if (faqDist <= 0 && sponsorsDist > 0) { //checks if you're in faq section
             console.log("WE ARE IN FAQ BABYYYY");
             $('nav ul li a').removeClass();
             $('#faqlink').addClass('active');
-        } else if (sponsorsDist <= 0 && organizersDist > 0) {
+        } else if (sponsorsDist <= 0 && organizersDist > 0) { //checks if you're in sponsors section
             $('nav ul li a').removeClass();
             $('#sponsorslink').addClass('active');
-        } else {
+        } else { // checks if you're in anywhere but the ones checked before, ie contact section
             $('nav ul li a').removeClass();
             $('#contactlink').addClass('active');
         }     
