26339a9141d41c82b8706c60f75073572be1ed6b
Gabriel Gomez <gabrielgomez@Gabriels-MBP.socal.rr.com>
2018-08-20 20:08:23 -0700

    implemented smooth jQuery smooth scroll and condensed faq js functions to a single function

--- cutie/landingapp/static/js/main.js
+++ cutie/landingapp/static/js/main.js
@@ -1,43 +1,36 @@
 //import jquery
 
 $(document).ready(function(){
-    $("#q1").click(function(){
-        $("#a1").slideToggle("slow");
+    $('.faqQuestion').click(function(){
+        $("#a" + this.id.charAt(1)).slideToggle("slow");
     });
 });
 
 $(document).ready(function(){
-    $("#q2").click(function(){
-        $("#a2").slideToggle("slow");
-    });
-});
-
-$(document).ready(function(){
-    $("#q3").click(function(){
-        $("#a3").slideToggle("slow");
-    });
-});
-
-$(document).ready(function(){
-    $("#q4").click(function(){
-        $("#a4").slideToggle("slow");
-    });
-});
-
-$(document).ready(function(){
-    $("#q5").click(function(){
-        $("#a5").slideToggle("slow");
-    });
-});
-
-$(document).ready(function(){
-    $("#q6").click(function(){
-        $("#a6").slideToggle("slow");
-    });
-});
-
-$(document).ready(function(){
-    $("#q7").click(function(){
-        $("#a7").slideToggle("slow");
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+            
+            if (target.length) {
+                event.preventDefault();
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top
+                    }, 
+                    1000,
+                    function() {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr("tabindex", "-1");
+                            $target.focus(); 
+                        }
+                    }
+                );
+            }
+        }
     });
 });
