b1a6806c688c7e4ecc04aea4a597676f2e582070
rachelquan <39972689+rachelquan@users.noreply.github.com>
2018-09-14 22:05:23 -0700

    nav links now highlight when at section titles as well

--- live/js/livepage.js
+++ live/js/livepage.js
@@ -20,38 +20,45 @@ var x = setInterval(function() {
 }, 1000);
 
 //changes active nav link on scroll
-$(document).ready(function() { //(document).ready: let file know that it is jQuery
+$(document).ready(function() {
     $(window).scroll(function(){
         var scrollTop       = $(window).scrollTop() + 10, // top of browser + 10px
             scheduleOffset    = $('#schedule').offset().top,
-            scheduleDist      = (scheduleOffset - scrollTop), // stores current distance between top of browser and "about" section
+            scheduleDist      = (scheduleOffset - scrollTop), // stores current distance between top of browser and "schedule" section
             mapOffset       = $('#map').offset().top,
-            mapDist         = (mapOffset - scrollTop), // stores current distance between top of browser and "faq" section
-            speakersOffset  = $('#speakers').offset().top,
-            speakersDist    = (speakersOffset - scrollTop); // stores current distance between top of browser and "sponsors" section
+            mapDist         = (mapOffset - scrollTop), // stores current distance between top of browser and "map" section
             resourcesOffset  = $('#resources').offset().top,
-            resourcesDist    = (resourcesOffset - scrollTop); // stores current distance between top of browser and "sponsors" section
+            resourcesDist    = (resourcesOffset - scrollTop); // stores current distance between top of browser and "resources" section
+            speakersOffset  = $('#speakers').offset().top,
+            speakersDist    = (speakersOffset - scrollTop); // stores current distance between top of browser and "speakers" section
             sponsorsOffset  = $('#sponsors').offset().top,
-            sponsorsDist    = (sponsorsOffset - scrollTop); // stores current distance between top of browser and "sponsors" section
+            sponsorsDist    = (sponsorsOffset - scrollTop); // stores current distance between top of browser and "speakers" section
 
-        if (scheduleDist > 0) { //checks if you're in home section
-            $('nav ul li a').removeClass();
-            $('#homelink').addClass('active');
-        } else if (scheduleDist < 0 && mapDist >= 0) { //checks if you're in schedule section
-            $('nav ul li a').removeClass();
-            $('#schedulelink').addClass('active');
-        } else if (mapDist < 0 && resourcesDist >= 0) { //checks if you're in map section
-            $('nav ul li a').removeClass();
-            $('#maplink').addClass('active');
-        } else if (resourcesDist < 0 && speakersDist >= 0) { //checks if you're in resources section
-            $('nav ul li a').removeClass();
-            $('#resourceslink').addClass('active');
-        } else if (speakersDist < 0 && sponsorsDist >= 0) { //checks if you're in speakers section
-              $('nav ul li a').removeClass();
-              $('#speakerslink').addClass('active');
-        } else if (sponsorsDist < 0 && !((window.innerHeight + window.scrollY) >= document.body.offsetHeight)) { //checks if you're in sponsors section, but haven't hit the bottom of page
-            $('nav ul li a').removeClass();
-            $('#sponsorslink').addClass('active');
-        }
-    });
-});
+            // console.log("===========");
+            // console.log("SCHEDULE DIST IS: " + scheduleDist);
+            // console.log("MAP DIST IS: " + mapDist);
+            // console.log("RESOURCES DIST IS: " + resourcesDist);
+            // console.log("SPEAKERS DIST IS: " + speakersDist);
+            // console.log("SPONSORS DIST IS: " + sponsorsDist);
+
+            if (scheduleDist > 19) { //checks if you're in home section
+                      $('nav ul li a').removeClass();
+                      $('#homelink').addClass('active');
+                  } else if (scheduleDist <= 19 && mapDist > 62) { //checks if you're in schedule section
+                      $('nav ul li a').removeClass();
+                      $('#schedulelink').addClass('active');
+                  } else if (mapDist <= 62 && resourcesDist > 88) { //checks if you're in map  section
+                      $('nav ul li a').removeClass();
+                      $('#maplink').addClass('active');
+                  } else if (resourcesDist <= 88 && speakersDist > 73) { //checks if you're in resources section
+                      $('nav ul li a').removeClass();
+                      $('#resourceslink').addClass('active');
+                  } else if (speakersDist <= 73 && sponsorsDist > 57) { //checks if you're in speakers section
+                      $('nav ul li a').removeClass();
+                      $('#speakerslink').addClass('active');
+                  } else if (sponsorsDist <= 57 && !((window.innerHeight + window.scrollY) >= document.body.offsetHeight)) { //checks if you're in sponsors section, but haven't hit the bottom of page
+                      $('nav ul li a').removeClass();
+                      $('#sponsorslink').addClass('active');
+                  }
+              });
+          });
