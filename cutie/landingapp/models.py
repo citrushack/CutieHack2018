866d8a7f2a405558e690f868eb1a4b65065cb601
Aditya Acharya <30582104+adialachar@users.noreply.github.com>
2018-09-24 20:20:30 -0700

    changed has perm and module perm to return True

--- cutie/landingapp/models.py
+++ cutie/landingapp/models.py
@@ -61,12 +61,12 @@ class MyUser(AbstractBaseUser):
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
         # Simplest possible answer: Yes, always
-        return False
+        return True
 
     def has_module_perms(self, app_label):
         "Does the user have permissions to view the app `app_label`?"
         # Simplest possible answer: Yes, always
-        return False
+        return True
 
     @property
     def is_staff(self):
