e04ee488a0271984382876264091e51f4dae72e7
Aditya Acharya <30582104+adialachar@users.noreply.github.com>
2018-10-02 01:57:04 -0700

    Update settings.py

--- cutie/cutie/settings.py
+++ cutie/cutie/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$dd0v0!7!h+x6_qondkdg6&k9-9w#@ejil$g6o%0yrd%qdran('
+# We have the secret key if you need it for the master branch! If you need it ask Ji Hwan or Aditya
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
